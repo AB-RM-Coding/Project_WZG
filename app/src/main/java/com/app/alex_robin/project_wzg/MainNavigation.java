e4554b9bf7e34140371f300f5d9e006cded02712
braunale <alexanderbraun95@aol.com>
2016-04-19 18:47:54 +0200

    Commit from the first created app version.

---
+++ app/src/main/java/com/app/alex_robin/project_wzg/MainNavigation.java
@@ -0,0 +1,16 @@
+package com.app.alex_robin.project_wzg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainNavigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_navigation);
+
+        //comment
+
+    }
+}
