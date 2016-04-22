115d81d8c1e219c607bb5c408afc84831dffb8bf
braunale <alexanderbraun95@aol.com>
2016-04-22 15:59:29 +0200

    adding Activities "CardDeck" and "QuoteCollection" and navigation button on the mainNavigation Activity

--- app/src/main/java/com/app/alex_robin/project_wzg/MainNavigation.java
+++ app/src/main/java/com/app/alex_robin/project_wzg/MainNavigation.java
@@ -1,21 +1,45 @@
 package com.app.alex_robin.project_wzg;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainNavigation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_navigation);
 
-        //comment
-        //comment2
+    }//end onCreate
 
 
-        //Comment over 9000
+    public void open_CardDeck(View view){
 
+        //Jumps into Activity CardDeck
+
+        Intent myIntent = new Intent(MainNavigation.this, CardDeck.class);
+        MainNavigation.this.startActivity(myIntent);
 
     }
+
+    public void open_QuoteCollection(View view){
+
+        //Jumps into Activity QuoteCollection
+
+        Intent myIntent = new Intent(MainNavigation.this, QuoteCollection.class);
+        MainNavigation.this.startActivity(myIntent);
+
+    }
+
+
+
+
+
+
+
+
 }
---
+++ app/src/main/java/com/app/alex_robin/project_wzg/QuoteCollection.java
@@ -0,0 +1,13 @@
+package com.app.alex_robin.project_wzg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuoteCollection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quote_collection);
+    }
+}
